83241ec97587071dd58f98caf23fd5951ba29c62
Cris <cristian_guher@outlook.com>
2024-01-19 20:34:30 -0600

    verificando commit en github

--- curso-1-logica-de-programacion-Sumergete-en-programacion-con-JavaScript/app.js
+++ curso-1-logica-de-programacion-Sumergete-en-programacion-con-JavaScript/app.js
@@ -2,7 +2,6 @@
 let numeroSecreto=Math.floor(Math.random()*10);
 let numeroUsuario=0;
 let intentos = 1;
-//let SingularOPlural= 'intento';
 let maximosIntentos=3;
 
 console.log(numeroSecreto)
@@ -11,7 +10,7 @@ while (numeroSecreto != numeroUsuario){
     console.log(numeroUsuario);
     //aqui se realiza la comparación
     if (numeroUsuario == numeroSecreto){
-        alert(`Acertaste, el numero es: ${numeroUsuario}. Lo hiciste en ${intentos} ${intentos ==1 ? "vez" :'veces'}`);
+        alert(`Acertaste, el numero es: ${numeroUsuario}. Lo hiciste en ${intentos} ${intentos == 1 ? 'intento':'intentos' }`);
         break;
     } else{
         if (numeroSecreto > numeroUsuario){
@@ -21,13 +20,11 @@ while (numeroSecreto != numeroUsuario){
             alert("Lo siento, el numero secreto es menor")
         }
         //Se incrementa el numero de intentos cada vez que no acertamos 
-        intentos= intentos +1;
-        SingularOPlural = "intentos";
+        intentos++;
         
         if (intentos> maximosIntentos){
-            alert(`lo siento, utilizaste tus ${maximosIntentos} ${intentos}`);
+            alert(`lo siento, utilizaste tus ${maximosIntentos} intentos`);
             break;
         }
-        //alert("Lo siento no acertaste el numero");
     }
 }
