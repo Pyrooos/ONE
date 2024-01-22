c4db9620df572133ebb537fda1be553d69a11d48
Cris <cristian_guher@outlook.com>
2024-01-22 16:47:50 -0600

    Se completa desafio 1. Version no final, puede modificarse.

---
+++ Lógica-de-programación-explorar-funciones-y-listas/Desafio-1/app.js
@@ -0,0 +1,26 @@
+//1 se esta trabajando sobre los documentos proporcionados
+
+//2
+let horaDelDesafio = document.querySelector("h1");
+titulo.innerHTML("Hora del desafío");
+
+//3
+function clickConsole(){
+    console.log("El botón fue clicado")
+}
+
+//4
+function ciudad(){
+    nombreCiudad= prompt("Nombra una ciudad de Brasil");
+    alert(`Estuve en ${nombreCiudad} y me acordé de ti`);
+}
+function ilove(){
+    alert("Yo amo JS");
+}
+
+function suma(){
+    primerDigito=parseInt(prompt("Por favor ingresa el primer digito para una suma"));
+    segundoDigito=parseInt(prompt("Introduce el segundo digito"));
+    resultado = primerDigito + segundoDigito;
+    alert(`El resultado de ${primerDigito} + ${segundoDigito} es igual a ${resultado} `)
+}
