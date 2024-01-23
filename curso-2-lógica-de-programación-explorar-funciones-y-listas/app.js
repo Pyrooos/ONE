6e222a9dd2a7c31f6da1814470559660cf7b03e9
Cris <cristian_guher@outlook.com>
2024-01-22 19:11:16 -0600

    Se crea un input de mejor manejar que el primer curso

---
+++ curso-2-lógica-de-programación-explorar-funciones-y-listas/app.js
@@ -0,0 +1,23 @@
+
+let numeroSecreto = generarNumeroSecreto();
+
+
+
+function asignarTextoElemento(elemento, texto){
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+    return;
+}
+function verificarIntento(){
+    let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);
+    console.log(numeroSecreto);
+    console.log(numeroDeUsuario);
+    console.log(numeroDeUsuario === numeroSecreto);
+    return;
+}
+
+function generarNumeroSecreto() {
+    return Math.floor(Math.random()*10)+1;  
+}
+asignarTextoElemento('h1', "Juego del numero secreto");
+asignarTextoElemento('p', "Indica un número del 1 al 10");
