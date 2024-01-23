65294dfe19fe474a94a630b5d49b60c0ca1c79d2
Cris <cristian_guher@outlook.com>
2024-01-22 18:38:47 -0600

    Se simplifica el codigo, reduciendo lineas agregando una funcion que defina parametros con esto consiguiendo escabilidad y una mayor comprension de codigo

--- Lógica-de-programación-explorar-funciones-y-listas/app.js
+++ Lógica-de-programación-explorar-funciones-y-listas/app.js
@@ -1,9 +1,10 @@
-let titulo = document.querySelector("h1");
-titulo.innerHTML = 'Juego del número secreto';
 
-let parrafo = document.querySelector('p');
-parrafo.innerHTML = "Indica un número del 1 al 10";
-
+function asignarTextoElemento(elemento, texto){
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+}
 function intentoDeUsuario(){
     alert("click");
 }
+asignarTextoElemento('h1', "Juego del numero secreto");
+asignarTextoElemento('p', "Indica un número del 1 al 100");
