426372a8c56757229e69cf0272756381a4102ff3
Cris <cristian_guher@outlook.com>
2024-01-23 20:03:19 -0600

    Se anaden particualirades del lenguaje sobre singular o plural

--- curso-2-lógica-de-programación-explorar-funciones-y-listas/app.js
+++ curso-2-lógica-de-programación-explorar-funciones-y-listas/app.js
@@ -1,7 +1,6 @@
 
 let numeroSecreto = generarNumeroSecreto();
-
-
+let contadorIntentos = 1;
 
 function asignarTextoElemento(elemento, texto){
     let elementoHTML = document.querySelector(elemento);
@@ -12,16 +11,16 @@ function verificarIntento(){
     let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);
     console.log(numeroSecreto);
     console.log(numeroDeUsuario);
-
+    console.log(contadorIntentos);
     if (numeroDeUsuario === numeroSecreto){
-        asignarTextoElemento('p', 'Acertaste el numero')
+        asignarTextoElemento('p', `Acertaste el numero en ${contadorIntentos} ${contadorIntentos == 1 ? "intento" : 'intentos'} `)
     }else {
         if (numeroDeUsuario < numeroSecreto) {
         asignarTextoElemento("p", "El número es mayor")  
         }else {
             asignarTextoElemento("p", "El número es menor")
         } 
-        
+        contadorIntentos++;
     }
     return;
 }
