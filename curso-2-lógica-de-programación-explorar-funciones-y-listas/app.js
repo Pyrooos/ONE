08cf4d977a2b773a5838e072b2706f1a6235cee7
Cris <cristian_guher@outlook.com>
2024-01-23 20:15:51 -0600

    en app de curso 2, se implementa el borrado automatico de la caja de texto en cuanto el usuario introduce un digito erroneo y ademas se activa el boton en caso de ganar el juego

--- curso-2-lógica-de-programación-explorar-funciones-y-listas/app.js
+++ curso-2-lógica-de-programación-explorar-funciones-y-listas/app.js
@@ -14,19 +14,27 @@ function verificarIntento(){
     console.log(contadorIntentos);
     if (numeroDeUsuario === numeroSecreto){
         asignarTextoElemento('p', `Acertaste el numero en ${contadorIntentos} ${contadorIntentos == 1 ? "intento" : 'intentos'} `)
+        document.getElementById('reiniciar').removeAttribute("disabled")
     }else {
+        //El usuario no acerto
         if (numeroDeUsuario < numeroSecreto) {
         asignarTextoElemento("p", "El número es mayor")  
         }else {
             asignarTextoElemento("p", "El número es menor")
-        } 
+        }
+        limpiarCaja(); 
         contadorIntentos++;
     }
     return;
 }
 
+function limpiarCaja(){
+    document.querySelector('#valorUsuario').value="";
+}
+
 function generarNumeroSecreto() {
     return Math.floor(Math.random()*10)+1;  
 }
+
 asignarTextoElemento('h1', "Juego del numero secreto");
 asignarTextoElemento('p', "Indica un número del 1 al 10");
