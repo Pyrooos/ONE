cf5af76d55e45ba2c8faedd1bc632a5a8ae10d0b
Cris <cristian_guher@outlook.com>
2024-01-25 14:22:38 -0600

    Hay un error en la logica y no encuentro donde, regresando a una version anteiro

--- curso-2-lógica-de-programación-explorar-funciones-y-listas/app.js
+++ curso-2-lógica-de-programación-explorar-funciones-y-listas/app.js
@@ -1,14 +1,15 @@
 
 let numeroSecreto = generarNumeroSecreto();
-let contadorIntentos = 0;
+let contadorIntentos;
 let contadorMaximoIntentos =5;
+let listaNumerosSorteados = [];
 
 function asignarTextoElemento(elemento, texto){
     let elementoHTML = document.querySelector(elemento);
     elementoHTML.innerHTML = texto;
     return;
 }
-/*function verificarIntento(){
+function verificarIntento(){
     let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);
     console.log(numeroSecreto);
     console.log(numeroDeUsuario);
@@ -17,7 +18,7 @@ function asignarTextoElemento(elemento, texto){
         if (numeroDeUsuario === numeroSecreto){
             asignarTextoElemento('p', `Acertaste el numero en ${contadorIntentos} ${contadorIntentos == 1 ? "intento" : 'intentos'} `)
             document.getElementById('reiniciar').removeAttribute("disabled")
-        }else {
+        } else {
             //El usuario no acerto
             if (numeroDeUsuario < numeroSecreto) {
             asignarTextoElemento("p", "El número es mayor")  
@@ -29,49 +30,36 @@ function asignarTextoElemento(elemento, texto){
         }
     return;
 }
-*/
-
-function verificarIntento(){
-    let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);
-
-    while(numeroDeUsuario !== numeroSecreto){
-        if (numeroDeUsuario === numeroSecreto){
-          
-            asignarTextoElemento('p', `Acertaste el numero en ${contadorIntentos} ${contadorIntentos == 1 ? "intento" : 'intentos'} `)
-            document.getElementById('reiniciar').removeAttribute("disabled")
-        }else {
-            if(numeroDeUsuario > numeroSecreto){
-                asignarTextoElemento('p', 'El numero secreto es menor')
-            } else if ( numeroDeUsuario < numeroSecreto){
-                asignarTextoElemento('p', 'El numero secreto es mayor')
-        }
-        limpiarCaja(); 
-    contadorIntentos++;
-    }if (contadorIntentos === contadorMaximoIntentos){
-        asignarTextoElemento('p', 'Lo siento, agotaste tus intentos. Intentalo de nuevo')
-        break;
-
-        
-
-    }
-    }
-    
-    return;
-}
-
 function condicionesIniciales(){
     asignarTextoElemento('h1', "Juego del numero secreto");
     asignarTextoElemento('p', "Indica un número del 1 al 10");
     numeroSecreto = generarNumeroSecreto();
     contadorIntentos = 1;
+    
 }
 function limpiarCaja(){
     document.querySelector('#valorUsuario').value="";
 }
 
 function generarNumeroSecreto() {
-    return Math.floor(Math.random()*10)+1;  
+    let numeroGenerado =  Math.floor(Math.random()* 10 )+1;
+
+    console.log(numeroGenerado);
+    console.log(listaNumerosSorteados);
+    //Si ya sorteamos todos los números
+    if (listaNumerosSorteados.length == numeroMaximo) {
+        asignarTextoElemento('p','Ya se sortearon todos los números posibles');
+    } else {
+        //Si el numero generado está incluido en la lista 
+        if (listaNumerosSorteados.includes(numeroGenerado)) {
+            return generarNumeroSecreto();
+        } else {
+            listaNumerosSorteados.push(numeroGenerado);
+            return numeroGenerado;
+        }
+    }
 }
+
 function reiniciarJuego(){
     //limpiar la caja
     limpiarCaja();
@@ -81,7 +69,7 @@ function reiniciarJuego(){
     //deshabilitar boton de nuevo juego
     
     //inicializar el numero de intentos
-    condicionesIniciales
+    
     document.querySelector('#reiniciar').setAttribute('disabled', 'true');
 }
 
