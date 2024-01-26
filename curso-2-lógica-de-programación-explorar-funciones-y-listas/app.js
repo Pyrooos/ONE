8dc05780d7485635adddbf980f5f883f6dcc4338
Cris <cristian_guher@outlook.com>
2024-01-25 18:52:43 -0600

    Se limita la recursividad a una cantidad de numeros para evitar bucles infinos

--- curso-2-lógica-de-programación-explorar-funciones-y-listas/app.js
+++ curso-2-lógica-de-programación-explorar-funciones-y-listas/app.js
@@ -1,15 +1,9 @@
 
-<<<<<<< HEAD
 let contadorIntentos = 0;
 let listaNumerosSorteados = [];
 let numeroSecreto = 0;
 let numeroMaximoDeIntentos = 5;
-=======
-let numeroSecreto = generarNumeroSecreto();
-let contadorIntentos;
-let contadorMaximoIntentos =5;
-let listaNumerosSorteados = [];
->>>>>>> cf5af76d55e45ba2c8faedd1bc632a5a8ae10d0b
+let numeroMaximoPosible = 10;
 
 function asignarTextoElemento(elemento, texto){
     let elementoHTML = document.querySelector(elemento);
@@ -18,9 +12,8 @@ function asignarTextoElemento(elemento, texto){
 }
 function verificarIntento(){
     let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);
-<<<<<<< HEAD
     
-    console.log(`numero de secreto :${numeroSecreto}`);
+    
     console.log(`numero de usuario :${numeroDeUsuario}`);
     console.log(`numero de intentos: ${contadorIntentos}`);
     console.log(`Numero Maximo de intentos ${numeroMaximoDeIntentos}`)
@@ -46,26 +39,6 @@ function verificarIntento(){
             document.getElementById('reiniciar').removeAttribute("disabled");
         }break;      
     }               
-=======
-    console.log(numeroSecreto);
-    console.log(numeroDeUsuario);
-    console.log(contadorIntentos);
-    
-        if (numeroDeUsuario === numeroSecreto){
-            asignarTextoElemento('p', `Acertaste el numero en ${contadorIntentos} ${contadorIntentos == 1 ? "intento" : 'intentos'} `)
-            document.getElementById('reiniciar').removeAttribute("disabled")
-        } else {
-            //El usuario no acerto
-            if (numeroDeUsuario < numeroSecreto) {
-            asignarTextoElemento("p", "El número es mayor")  
-            }else {
-                asignarTextoElemento("p", "El número es menor")
-            }
-            limpiarCaja(); 
-            contadorIntentos++;
-        }
-    return;
->>>>>>> cf5af76d55e45ba2c8faedd1bc632a5a8ae10d0b
 }
 
 function condicionesIniciales(){
@@ -74,6 +47,7 @@ function condicionesIniciales(){
     asignarTextoElemento('p', `Indica un número del 1 al 10, solo tienes ${numeroMaximoDeIntentos} intentos`);
     numeroSecreto = generarNumeroSecreto();
     contadorIntentos = 1;
+    console.log(`numero secreto: ${numeroSecreto}`)
     
 }
 function limpiarCaja(){
@@ -81,35 +55,21 @@ function limpiarCaja(){
 }
 
 function generarNumeroSecreto() {
-<<<<<<< HEAD
-    
-    let numeroGenerado = Math.floor(Math.random()*10)+1; 
-    
-    if (listaNumerosSorteados.includes(numeroGenerado)){
-        return generarNumeroSecreto();
-    } else { 
-        listaNumerosSorteados.push(numeroGenerado)
-        return numeroGenerado;
-=======
-    let numeroGenerado =  Math.floor(Math.random()* 10 )+1;
+    if (listaNumerosSorteados.length == numeroMaximoPosible){
+        asignarTextoElemento('p', 'todos los numeros posibles ya han sido usados')
 
-    console.log(numeroGenerado);
-    console.log(listaNumerosSorteados);
-    //Si ya sorteamos todos los números
-    if (listaNumerosSorteados.length == numeroMaximo) {
-        asignarTextoElemento('p','Ya se sortearon todos los números posibles');
-    } else {
-        //Si el numero generado está incluido en la lista 
-        if (listaNumerosSorteados.includes(numeroGenerado)) {
+
+    } else{
+        let numeroGenerado = Math.floor(Math.random()*10)+1; 
+        
+        if (listaNumerosSorteados.includes(numeroGenerado)){
             return generarNumeroSecreto();
-        } else {
-            listaNumerosSorteados.push(numeroGenerado);
+        } else { 
+            listaNumerosSorteados.push(numeroGenerado)
             return numeroGenerado;
         }
->>>>>>> cf5af76d55e45ba2c8faedd1bc632a5a8ae10d0b
     }
 }
-
 function reiniciarJuego(){
     //limpiar la caja
     limpiarCaja();
