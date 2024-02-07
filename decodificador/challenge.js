46feca67fdd4a66603639cb90b746ce2f5b763e5
Cris <cristian_guher@outlook.com>
2024-02-06 22:33:22 -0600

    se avanzo codificador decodificador, falla en logica al codificar. revisando

--- decodificador/challenge.js
+++ decodificador/challenge.js
@@ -1,10 +1,64 @@
 
 
-function codificador(parrafo){
-    var textoModificado=parrafo.replace(/a/g,'Er' );
-    return { original: parrafo, modificado: textoModificado };
+function codificador(parrafo) {
+    let modificado = parrafo;
+
+    if (parrafo.includes('a')) {
+        parrafo = modificado.replace(/a/g, 'ai');
+    } 
+
+    if (parrafo.includes('e')) {
+        
+        parrafo = modificado.replace(/e/g, 'enter');
+    }
+    if (parrafo.includes('i')){
+        parrafo = modificado.replace(/i/g, 'imes');
+    }
+    if (parrafo.includes('o')){
+        parrafo = modificado.replace(/o/g, 'ober');
+    }
+    
+    if (parrafo.includes('u')){
+        parrafo = modificado.replace(/u/g, 'ufat');
+    }
+    return { original: parrafo, codificado: modificado };
 }
 
-var resultado= codificador('agua de horchata')
-console.log(codificador(resultado.modificado));
-console.log(codificador(resultado.original));
+
+
+function descodificador(parrafoCodificado) {
+    let modificado = parrafoCodificado;
+
+    if (parrafoCodificado.includes('ufat')) {
+        modificado = modificado.replace(/ufat/g, 'u');
+    }
+
+    if (parrafoCodificado.includes('ober')) {
+        modificado = modificado.replace(/ober/g, 'o');
+    }
+
+    if (parrafoCodificado.includes('imes')) {
+        modificado = modificado.replace(/imes/g, 'i');
+    }
+
+    if (parrafoCodificado.includes('enter')) {
+        modificado = modificado.replace(/enter/g, 'e');
+    }
+
+    if (parrafoCodificado.includes('ai')) {
+        modificado = modificado.replace(/ai/g, 'a');
+    }
+
+    return { original: modificado, codificado: parrafoCodificado };
+}
+//hay que revisar la logica porque no esta codificando la primera parte
+
+
+var resultado = codificador('felicidades por enfrentar este desafio y haberlo concluido con exito!');
+console.log(resultado.codificado);
+console.log(resultado.original);
+var resultadoCodificacion = codificador('fenterlimescimesdaidenters poberr enternfrenterntair enterstenter dentersaifimesober y haibenterrlober cobernclufatimesdober cobern enterximestober!');
+console.log(resultadoCodificacion.codificado);
+
+var resultadoDescodificacion = descodificador(resultadoCodificacion.codificado);
+console.log(resultadoDescodificacion.original);
