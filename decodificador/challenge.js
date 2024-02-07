14bdb03194e4ffebb1cc866504afdc50b2b46d0c
Cris <cristian_guher@outlook.com>
2024-02-06 20:49:03 -0600

    se tiene prototipo de funcion, hay que probar para ver si de usa, modifica o deshecha

--- decodificador/challenge.js
+++ decodificador/challenge.js
@@ -1,9 +1,10 @@
 
 
-function parrafoModificado(palabra){
-    palabramodificada= palabra.replace(/a/,'Er' )
-    return palabramodificada
+function codificador(parrafo){
+    var textoModificado=parrafo.replace(/a/g,'Er' );
+    return { original: parrafo, modificado: textoModificado };
 }
 
-
-console.log(parrafoModificado('agua'))
+var resultado= codificador('agua de horchata')
+console.log(codificador(resultado.modificado));
+console.log(codificador(resultado.original));
